47edf09e1a6507645306d31bd2b841fb4c52fa46
Mariusz Lyszcz <mariuszly@gmail.com>
2022-12-03 16:39:09 +0100

    added function in JS

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,15 @@
-let naglowek = document.querySelector(".naglowek")
-let przycisk = document.querySelector(".przycisk")
-
-przycisk.addEventListener("click", () => {
-    naglowek.classList.toggle("blue")
-})
-
-console.log("To jest moja druga strona internetowa")
+{
+    const info = () => {
+        console.log("To jest moja kolejna strona internetowa")
+    }
+    const toggleBackgroundHeader = () => {
+        const naglowek = document.querySelector(".naglowek");
+        naglowek.classList.toggle("blue");
+    }
+    const init = () => {
+        const przycisk = document.querySelector(".przycisk");
+        przycisk.addEventListener("click", toggleBackgroundHeader);
+        info();
+    }
+    init();
+}
