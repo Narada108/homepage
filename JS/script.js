cc7aa17488752a12e372f8391350286ff9899fe8
Mariusz Lyszcz <mariuszly@gmail.com>
2022-12-07 21:27:04 +0100

    added empty lines between functions

--- JS/script.js
+++ JS/script.js
@@ -2,14 +2,17 @@
     const info = () => {
         console.log("To jest moja kolejna strona internetowa")
     }
+
     const toggleBackgroundHeader = () => {
         const naglowek = document.querySelector(".naglowek");
         naglowek.classList.toggle("blue");
     }
+
     const init = () => {
         const przycisk = document.querySelector(".przycisk");
         przycisk.addEventListener("click", toggleBackgroundHeader);
         info();
     }
+    
     init();
 }
